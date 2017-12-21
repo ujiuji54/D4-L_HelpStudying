72ac745dcd7fa06011349fadfcb267eda2cfb203
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2017-12-21 12:27:57 +0900

    cardのメンバ書いた

--- cardlist.hpp
+++ cardlist.hpp
@@ -1,12 +1,20 @@
 #pragma once
 
 #include<iostream>
+#include<string>
+#include<vector>
 using namespace std;
 
 struct card{
-	
+	string name;
+	string mean;
+	int corrent_num;
+	int incorrect_num;
 };
 
 class cardlist{
-	
+private:
+	vector<struct card> card;
+public:
+	void 
 };
