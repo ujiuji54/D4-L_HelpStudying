83e0365c301a9424e0332270fdf30dfc3ce8fa50
24830 <yuukl248@icloud.com>
2017-12-21 12:30:10 +0900

    はじめの一歩

--- problem.hpp
+++ problem.hpp
@@ -1,8 +1,28 @@
 #pragma once
 
 #include<iostream>
+#include<string>
 using namespace std;
 
 class problem{
-		
+	string word["apple","banana","cow","egg","orange"];
+	string mean["りんご","ばなな","うし","たまご","おれんじ"];
+	string input;
+	int i,correct,incorrect;
+
+	for(i=0;i<5;i++){
+		cout << "第",i,"問" << endl;
+		cout << mean[i] <<endl;
+		cout << "上記の意味に応じた英単語を入力してください。" <<endl;
+		cin >> input;
+		if(word[i]==input){
+			cout << "正解！！" << endl;
+			correct++;
+		}else{
+			cout << "はい、違ーう！！\nわっかりやすい間違いをしてくれてありがとう！！" << endl;
+			incorrect++;
+		}
+	}
+	cout << "正解数  ",correct << endl;
+	cout << "不正解数",incorrect << endl;
 };
