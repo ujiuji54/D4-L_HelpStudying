ba727a3194058b9edbe8dbeb3453f35df96a7582
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-01-11 12:10:18 +0900

    method

---
+++ cardlist.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include"cardlist.hpp"
+using namespace std;
+
+cardlist::void make_card(){
+	
+}
+
+cardlist::void remove_card(){
+	
+}
+
+cardlist::void remove_user(){
+	
+}
+
+cardlist::string get_word(int num){
+	
+}
+
+cardlist::string get_mean(int num){
+	
+}
--- cardlist.hpp
+++ cardlist.hpp
@@ -8,13 +8,20 @@ using namespace std;
 struct card{
 	string name;
 	string mean;
-	int corrent_num;
+	int correct_num;
 	int incorrect_num;
+	vector<string> id;
+	vector<int> user_correct_num;
+	vector<int> user_incorrect_num;
 };
 
 class cardlist{
 private:
 	vector<struct card> card;
 public:
-	void 
+	void make_card();
+	void remove_card();
+	void remove_user();
+	string get_word(int num);
+	string get_mean(int num);
 };
