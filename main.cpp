4c955ee6bf609c7d9b47f60c18a09d58dfd88df6
nanami0627 <nanami.zeon98@gmail.com>
2018-01-18 11:12:48 +0900

    UI

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include<iostream>
 #include"cardlist.hpp"
 #include"userlist.hpp"
+#include"problem.hpp"
 using namespace std;
+int a;
 
 int main() {
-	
+	userlist userlist;	
+	string name;
+	string id;
+	id=userlist.login(name,id);
+
+	while(a!=3){
+		cout << "項目を入力してください　1=データベース閲覧　2=テスト　3=終了" <<endl;
+		cin >> a;
+		switch(a){
+			case 1:
+					cardlist(id);
+					break;
+			case 2:
+					plobrem.start(id);
+					break;
+			case 3:
+					break;
+				}
+	}
+
 }
