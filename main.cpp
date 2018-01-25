6359c40e7b7c4601b576a33724c7f47d880696f6
nanami0627 <nanami.zeon98@gmail.com>
2018-01-25 12:21:19 +0900

    tangotuika_tuika

--- main.cpp
+++ main.cpp
@@ -8,24 +8,41 @@ int main() {
 	int a,b;
 	cardlist cardlist;
 	USERLIST userlist;
-	string id;
+	string name,mean,id;
 	id=userlist.login();//ユーザ管理担当が作成
 
 	while(a!=4){
 		cout << "ようこそ！　L班の完璧な英単語システムへ" <<endl;
-		cout << "項目を入力してください　1=データベース閲覧　2=テスト　3=データ削除　4=終了" <<endl;
+		cout << "項目を入力してください　1=データベース　2=テスト　3=データ削除　4=終了" <<endl;
 		cin >> a;
 		switch(a){
 			case 1:
-					cout << "使用するデータベースを選択してください　1=共通　2=個人" << endl;
+					cout << "使用する機能を選択してください　1=閲覧　2=単語追加　3=単語削除" << endl;
 					cin >> b;
-					if (b==1){ 
-						cardlist.display();//データベース担当が作成
-						break;
-					}else{
-						cardlist.display(id);//データベース担当が作成
-						break;
+					switch(b){
+						case 1:
+							cout << "使用する項目を選択してください　1=共通　2=個人" << endl;
+							cin >> b;
+							if (b==1){ 
+								cardlist.display();//データベース担当が作成
+								break;
+							}else{
+								cardlist.display(id);//データベース担当が作成
+								break;
+							}
+						case 2:
+							cout << "名前　意味　IDの順に入力してください" << endl;
+							cin >> name;
+							cin >> mean;
+							cin >> id;
+							cardlist.make_card(name,mean,id);
+							break;
+						case 3:
+							cout << "名前　IDの順に入力してください" << endl;
+							void remove_card(string name,string id);//指定した単語のcardを消す。引数(単語,ユーザのid)
+							break;
 					}
+					break;
 			case 2:
 					//plobrem.start(id,cardlist);//テスト担当が作成
 					break;
@@ -39,10 +56,11 @@ int main() {
 						cout << "データ削除をキャンセルします" << endl;
 						break;
 					}
+			case 4:
+					break;
 			default:
 					cout  << "ちーがーうーだーろー。違うだろぉ！！！" << endl;
 					break; 
-				
 		}
 	}
 	return 0;
