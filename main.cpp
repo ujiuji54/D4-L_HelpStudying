1de3681d62c94dc1b0cf4e190255a861daac548e
nanami0627 <nanami.zeon98@gmail.com>
2018-01-25 11:49:45 +0900

    syusei

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include"cardlist.hpp"
 #include"userlist.hpp"
-#include"problem.hpp"
+//#include"problem.hpp"
 using namespace std;
 
 int main() {
@@ -20,20 +20,20 @@ int main() {
 					cout << "使用するデータベースを選択してください　1=共通　2=個人" << endl;
 					cin >> b;
 					if (b==1){ 
-						cardlist.display()//データベース担当が作成
+						cardlist.display();//データベース担当が作成
 						break;
 					}else{
 						cardlist.display(id);//データベース担当が作成
 						break;
 					}
 			case 2:
-					plobrem.start(id,cardlist);//テスト担当が作成
+					//plobrem.start(id,cardlist);//テスト担当が作成
 					break;
 			case 3:
 					cout << "本当にユーザーデータを削除してよ、よろしいか？　1=よろしい 2=よろしくない" << endl;
 					cin >> b;
 					if (b==1){ 
-						userlist.delete_user;//ユーザ管理担当が作成
+						userlist.delete_user();//ユーザ管理担当が作成
 						break;
 					}else{
 						cout << "データ削除をキャンセルします" << endl;
