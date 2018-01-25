d10ecea9f8f98861ef26d843ed166f03590ade9c
nanami0627 <nanami.zeon98@gmail.com>
2018-01-25 11:15:17 +0900

    UI

--- main.cpp
+++ main.cpp
@@ -6,24 +6,44 @@ using namespace std;
 int a;
 
 int main() {
-	userlist userlist;	
-	string name;
+	cardlist cardlist;
+	USERLIST userlist;
 	string id;
-	id=userlist.login(name,id);
+	id=userlist.login();//ユーザ管理担当が作成
 
 	while(a!=3){
-		cout << "項目を入力してください　1=データベース閲覧　2=テスト　3=終了" <<endl;
+		cout << "ようこそ！　L班の完璧な英単語システムへ" <<endl;
+		cout << "項目を入力してください　1=データベース閲覧　2=テスト　3=データ削除　4=終了" <<endl;
 		cin >> a;
 		switch(a){
 			case 1:
-					cardlist(id);
-					break;
+					cout << "使用するデータベースを選択してください　1=共通　2=個人" << endl;
+					cin >> b;
+					if (b==1){ 
+						cardlist.display()//データベース担当が作成
+						break;
+					}else{
+						cardlist.display(id);//データベース担当が作成
+						break;
+					}
 			case 2:
-					plobrem.start(id);
+					plobrem.start(id,cardlist);//テスト担当が作成
 					break;
 			case 3:
-					break;
-				}
+					cout << "本当にユーザーデータを削除してよ、よろしいか？　1=よろしい 2=よろしくない" << endl;
+					cin >> b;
+					if (b==1){ 
+						userlist.delete_user;//ユーザ管理担当が作成
+						break;
+					}else{
+						cout << "データ削除をキャンセルします" << endl;
+						break;
+					}
+			default:
+					cout  << "ちーがーうーだーろー。違うだろぉ！！！" << endl;
+					break; 
+				
+		}
 	}
-
+	return 0;
 }
